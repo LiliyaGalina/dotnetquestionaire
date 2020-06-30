0977ce82dc8ffa84f7e588d87c32cc6c613779a1
LiliyaGalina <lilianna.galina@ukr.net>
2020-06-30 20:28:02 +0300

    Init

---
+++ PassingArguments/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace PassingArguments
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var pointAClass = new PointAsClass(2, 2);
+            var pointBStruct = new PointAsStruct(2, 2);
+
+            ChangeClassPointSomehow(pointAClass);
+            ChangeStructPointSomehow(pointBStruct);
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"PointAsClass-> x:{pointAClass.X}, y:{pointAClass.Y}");
+            Console.WriteLine($"PointAsStruct-> x:{pointBStruct.X}, y:{pointBStruct.Y}");
+            Console.ResetColor();
+
+        }
+
+
+        static void ChangeClassPointSomehow( PointAsClass point)
+        {
+           point.X = 3;
+           point.Y = 3;
+        }
+        static void ChangeStructPointSomehow(PointAsStruct point)
+        {
+            point.X = 3;
+            point.Y = 3;
+        }
+
+
+    }
+
+    interface IPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+
+    class PointAsClass : IPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public PointAsClass(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    struct PointAsStruct : IPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public PointAsStruct(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+
+
+}
