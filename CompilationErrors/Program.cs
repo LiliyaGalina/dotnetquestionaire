47ac1950bef4ae122d59b86c048b7fe26a30a95e
LiliyaGalina <lilianna.galina@ukr.net>
2020-06-30 20:39:58 +0300

    Add compilation errors project

---
+++ CompilationErrors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CompilationErrors
+{
+    //пояснити і виправити всі помилки компіляції
+    class Program
+    {
+        const TimeSpan severalMinutes = TimeSpan.FromMinutes(3);
+
+        static void Main(string[] args)
+        {
+            var result = SomeFunction(DateTime.Now);
+
+            Console.WriteLine(result);
+        }
+
+        void SomeFunction(DateTime dat = DateTime.Now)
+        {
+            return dat.Add(severalMinutes);
+        }
+
+    }
+}
